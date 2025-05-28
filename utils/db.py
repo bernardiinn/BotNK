ccac25733026e563628397dfb82616c5da0cda18
Bernardo Martins <bernardo.lamartins@gmail.com>
2025-05-28 15:25:54 +0100

    DB Railway

--- utils/db.py
+++ utils/db.py
@@ -2,6 +2,5 @@ import os
 import sqlite3
 
 def get_db_connection():
-    base_dir = os.path.dirname(os.path.abspath(__file__))  # utils/
-    db_path = os.path.join(base_dir, "..", "relatorio.db")  # volta pra raiz do projeto
+    db_path = os.getenv("DB_PATH", "relatorio.db")  # Usa /data/relatorio.db na Railway, fallback para local
     return sqlite3.connect(db_path)
